4137380397cb98eb15588f46cefde7c721b97d98
kjetilja <none@none>
2001-12-06 15:34:09 +0000

    preliminary setup file to ease build & install

---
+++ setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+# $Id$
+
+"""Setup script for Psyco, the Python specializing compiler"""
+
+import os, sys
+from distutils.core import setup
+from distutils.extension import Extension
+
+# This one is sufficient right now
+src = ["c/hack.c"]
+
+###############################################################################
+
+setup (	name="psyco",
+      	version="0.3.4",
+      	description="Psyco, the Python specializing compiler",
+      	author="Armin Rigo",
+      	url="http://psyco.sourceforge.net/",
+      	ext_modules=[Extension(name="_psyco",
+                               sources=src,
+                               include_dirs=['c'])]
+        )
