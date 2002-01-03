85320a355b684b17082931f9e8e419b87bbce12f
kjetilja <none@none>
2002-01-03 11:54:30 +0000

    Setup for individually compiling each source file.  By default, however, psyco is compiled with good old hack.c in order to be debuggable with gdb and to avoid cluttering the namespace of Python itself.  So, this is a very non-intrusive update.

--- setup.py
+++ setup.py
@@ -1,16 +1,45 @@
 #! /usr/bin/env python
 
-# $Id$
-
 """Setup script for Psyco, the Python specializing compiler"""
 
 from distutils.core import setup
 from distutils.extension import Extension
 
-# This one is sufficient right now
-src = ["c/hack.c"]
 
-###############################################################################
+src = [
+    "c/Python/pycompiler.c",
+    "c/Python/pbltinmodule.c",
+    "c/Objects/pabstract.c",
+    "c/Objects/pdictobject.c",
+    "c/Objects/pfuncobject.c",
+    "c/Objects/pintobject.c",
+    "c/Objects/piterobject.c",
+    "c/Objects/plistobject.c",
+    "c/Objects/plongobject.c",
+    "c/Objects/pmethodobject.c",
+    "c/Objects/pobject.c",
+    "c/Objects/psycofuncobject.c",
+    "c/Objects/pstringobject.c",
+    "c/Objects/ptupleobject.c",
+    "c/Objects/pclassobject.c",
+    "c/Objects/pdescrobject.c",
+    "c/Objects/pstructmember.c",
+    "c/linuxmemchk.c",
+    "c/codemanager.c",
+    "c/dispatcher.c",
+    "c/processor.c",
+    "c/vcompiler.c",
+    "c/mergepoints.c",
+    "c/pycencoding.c",
+    "c/psyco.c"
+    ]
+
+
+# Psyco can be compiled and installed using the src list above, but
+# we need statics for gdb and to avoid cluttering the namespace
+# of Python itself, so for now we just use good old hack.c.
+src = ['c/hack.c']
+
 
 setup (	name="psyco",
       	version="0.3.4",
@@ -18,6 +47,5 @@ setup (	name="psyco",
       	author="Armin Rigo",
       	url="http://psyco.sourceforge.net/",
       	ext_modules=[Extension(name="_psyco",
-                               sources=src,
-                               include_dirs=['c'])]
+                               sources=src)]
         )
