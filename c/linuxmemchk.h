bacd116e05c02456c98107300c3b424ba5bf6ec5
kjetilja <none@none>
2002-01-03 11:22:46 +0000

    Only include macro redefinitions if HEAVY_MEM_CHECK is defined.

--- c/linuxmemchk.h
+++ c/linuxmemchk.h
@@ -1,5 +1,6 @@
-#include <Python.h>
+#ifdef HEAVY_MEM_CHECK
 
+#include <Python.h>
 #undef PyCore_MALLOC
 #undef PyCore_REALLOC
 #undef PyCore_FREE
@@ -11,3 +12,5 @@ EXTERNFN void* memchk_ef_realloc(void* data, int nsize);
 #define PyCore_MALLOC   memchk_ef_malloc
 #define PyCore_REALLOC  memchk_ef_realloc
 #define PyCore_FREE     memchk_ef_free
+
+#endif
