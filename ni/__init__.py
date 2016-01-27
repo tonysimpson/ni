606c70eb7404cbc5868e0ff36ee175d19f7ffc13
Tony Simpson <agjasimpson@gmail.com>
2016-01-27 22:06:04 +0000

    Added proxy and bind and a timeing function

--- ni/__init__.py
+++ ni/__init__.py
@@ -1,9 +1,24 @@
 import psyco
+from psyco import proxy, bind
+from time import time as get_time
 
 def engage():
-    """Turn on optimisation"""
+    """Turn on optimisation
+    
+    """
     psyco.full()
 
 def disengage():
-    """Turn off optimisation"""
+    """Turn off optimisation
+    
+    """
     psyco.stop()
+    
+def timeit_iter(func):
+    """Yield func call time in seconds _forever_
+    
+    """
+    while True:
+        st = get_time()
+        func()
+        yield get_time() - st
