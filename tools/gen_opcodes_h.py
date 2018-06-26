7425fdbcaf5742b1b0e4eab1da229e46a861deb2
Tony Simpson <agjasimpson@gmail.com>
2018-06-26 13:14:30 +0100

    Forgot to commit change

--- tools/gen_opcodes_h.py
+++ tools/gen_opcodes_h.py
@@ -1,6 +1,6 @@
 import opcode
 
-f = open('c/opcodes.h', 'w');
+f = open('../c/opcodes.h', 'w');
 f.write('#ifndef _OPCODES_H\n')
 f.write('#define _OPCODES_H\n')
 f.write('static const char* opcode_names [] = {\n')
